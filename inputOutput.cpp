ffed89e7cb937ec6adcdac4183433d29900aa04b
Siddesh-G-H <siddeshghsiddesh@gmail.com>
2024-10-26 23:37:47 +0530

    FIrst C++ file

---
+++ inputOutput.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+ using namespace std;
+ int main()
+ {
+    string name;
+    cout<<"What is your name? \n";
+    cin>>name;
+
+    int age;
+    cout<<"Enter your age >> ";
+    cin>>age;
+    cout<<"Hello "<<name<<endl<<"Nice to meet you"<<endl<<"your age is "<<age;
+    return 0;
+ }
